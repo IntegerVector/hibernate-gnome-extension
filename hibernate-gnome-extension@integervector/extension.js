1e2bec57eb6a58bcf66b3ffd025540c817fb1ae6
IntegerVector <andrew.ryban@gmail.com>
2021-04-17 01:31:26 +0300

    extesion body finished

--- hibernate-gnome-extension@integervector/extension.js
+++ hibernate-gnome-extension@integervector/extension.js
@@ -1,25 +1,54 @@
-const St = imports.gi.St;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+const Util = imports.misc.util;
 const Main = imports.ui.main;
 
-let hibernateButton;
+const AggregateMenu = Main.panel.statusArea.aggregateMenu;
+
+const scripts = {
+  hibernate: 'hibernate.sh'
+};
+
+let hibernateLabel = '';
+
+/**
+ * Extension events handlers functions:
+ */
 
 function init() {
-  hibernateButton = new St.Bin({
-    style_class: 'panel-button'
-  });
-
-  const buttonLabel = new St.Label({
-    style_class: 'button__label',
-    text: 'Hibernate'
-  });
-
-  hibernateButton.set_child(buttonLabel);
+  hibernateLabel = getHibernateLabel();
 }
 
 function enable() {
-
+  AggregateMenu._system._sessionSubMenu.menu.addAction(
+    hibernateLabel,
+    () => {
+      hibernateActionHandler();
+    }
+  );
 }
 
 function disable() {
+}
 
+/**
+ * Private functions:
+ */
+
+
+function getHibernateLabel() {
+  // To Do:
+  return 'Hibernate';
+}
+
+function hibernateActionHandler() {
+  const executionScriptPath = getScriptsFolderPath();
+  executeCommand([executionScriptPath + '/' + scripts.hibernate]);
+}
+
+function getScriptsFolderPath() {
+  return Me.dir.get_child('scripts').get_path();
+}
+
+function executeCommand(args) {
+  Util.spawn(args);
 }
